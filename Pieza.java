ee5633aa066a1926278df5876ece6d23181523ef
Mauricio Lopez <mauriciolopezosorio99@gmail.com>
2024-03-02 12:37:49 -0500

    Primero commit

---
+++ Pieza.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Pieza {
+    //atributos
+    private String tipo ;
+    private String color;
+    private int movimiento;
+
+    public Pieza (String tipo,String color){
+        this.tipo = tipo;
+        this.color = color;
+        this.movimiento = 3;
+    }
+
+    public static void main(String[] args){
+        
+    }
+
+}
